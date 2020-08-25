3f6ddb542bf167c6043973b5ead24d8764d97cf3
fercc1097 <fercc1097@gmail.com>
2020-08-25 18:30:16 -0500

    challenge done

--- src/index.js
+++ src/index.js
@@ -1,6 +1,22 @@
 function diamond(size) {
-  // your code
-  return;
+
+  if (size <= 1 || size % 2 === 0) return null
+  let diamond ='';
+  let char = '*'
+  let index=1;
+  while(index<size){
+    diamond+=' '.repeat((size-index)/2)+char.repeat(index)+"\n"
+    index+=2;
+  }
+  index=size;
+  var i=0;
+  while(index>=1){
+    diamond+=' '.repeat(i)+char.repeat(index)+"\n"
+    i++;
+    index-=2;
+  }
+  
+  return diamond;
 }
 
 module.exports = { diamond };
